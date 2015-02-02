eadf44166efe515a8b12f773c5f46fb3007feb46
Richard Newman <rnewman@twinql.com>
2015-02-02 13:33:23 -0800

    Refactoring shared classes.

--- src/main/java/org/mozilla/gecko/reading/ReadingListClient.java
+++ src/main/java/org/mozilla/gecko/reading/ReadingListClient.java
@@ -14,7 +14,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.mozilla.gecko.fxa.FxAccountConstants;
-import org.mozilla.gecko.reading.ReadingListClient.ReadingListStorageResponse;
 import org.mozilla.gecko.sync.ExtendedJSONObject;
 import org.mozilla.gecko.sync.NonObjectJSONException;
 import org.mozilla.gecko.sync.UnexpectedJSONException;
@@ -37,9 +36,14 @@ import ch.boye.httpclientandroidlib.impl.client.DefaultHttpClient;
 public class ReadingListClient {
   static final String LOG_TAG = ReadingListClient.class.getSimpleName();
 
-  private final String serviceURL;
+  private final URI serviceURI;
   private final AuthHeaderProvider auth;
 
+  private URI articlesURI;
+
+  /**
+   * A MozResponse that knows about all of the general RL-related headers, like Last-Modified.
+   */
   public static abstract class ReadingListResponse extends MozResponse {
     public ReadingListResponse(HttpResponse res) {
       super(res);
@@ -50,7 +54,9 @@ public class ReadingListClient {
     }
   }
 
-  // For a single record.
+  /**
+   * A storage response that contains a single record.
+   */
   public static class ReadingListRecordResponse extends ReadingListResponse {
     public static final ResponseFactory<ReadingListRecordResponse> FACTORY = new ResponseFactory<ReadingListRecordResponse>() {
       @Override
@@ -68,7 +74,9 @@ public class ReadingListClient {
     }
   }
 
-  // For multiple records.
+  /**
+   * A storage response that contains multiple records.
+   */
   public static class ReadingListStorageResponse extends ReadingListResponse {
     public static final ResponseFactory<ReadingListStorageResponse> FACTORY = new ResponseFactory<ReadingListStorageResponse>() {
       @Override
@@ -126,12 +134,14 @@ public class ReadingListClient {
     public T getResponse(HttpResponse r);
   }
 
-  private abstract class ReadingListResourceDelegate<T extends ReadingListResponse> extends BaseResourceDelegate {
+  private static abstract class ReadingListResourceDelegate<T extends ReadingListResponse> extends BaseResourceDelegate {
     private final ReadingListRecordDelegate delegate;
     private final ResponseFactory<T> factory;
+    private final AuthHeaderProvider auth;
 
-    public ReadingListResourceDelegate(Resource resource, ReadingListRecordDelegate recordDelegate, ResponseFactory<T> factory) {
+    public ReadingListResourceDelegate(Resource resource, AuthHeaderProvider auth, ReadingListRecordDelegate recordDelegate, ResponseFactory<T> factory) {
       super(resource);
+      this.auth = auth;
       this.delegate = recordDelegate;
       this.factory = factory;
     }
@@ -142,7 +152,7 @@ public class ReadingListClient {
 
     @Override
     public void handleHttpResponse(HttpResponse response) {
-      T resp = factory.getResponse(response);
+      final T resp = factory.getResponse(response);
       if (resp.wasSuccessful()) {
         onSuccess(resp);
       } else {
@@ -185,84 +195,58 @@ public class ReadingListClient {
   }
 
   /**
-   * Defines the parameters that can be added to a reading list fetch URI.
+   * An intermediate delegate class that handles all of the shared storage behavior,
+   * such as handling If-Modified-Since and 304 responses.
    */
-  public static class FetchSpec {
-    private final String queryString;
-    private FetchSpec(final String q) {
-      this.queryString = q;
+  private static abstract class ReadingListRecordResourceDelegate<T extends ReadingListResponse> extends ReadingListResourceDelegate<T> {
+    private final ReadingListRecordDelegate delegate;
+    private final long ifModifiedSince;
+
+    public ReadingListRecordResourceDelegate(Resource resource,
+                                             AuthHeaderProvider auth,
+                                             ReadingListRecordDelegate delegate,
+                                             ResponseFactory<T> factory,
+                                             long ifModifiedSince) {
+      super(resource, auth, delegate, factory);
+      this.delegate = delegate;
+      this.ifModifiedSince = ifModifiedSince;
     }
 
-    public URI getURI(String server, String path) throws URISyntaxException {
-      return new URI(server + path + "?" + queryString);
+    @Override
+    void onNotModified(T resp) {
+      delegate.onComplete();
     }
 
-    public class Builder {
-      final StringBuilder b = new StringBuilder();
-      boolean first = true;
+    @Override
+    void onFailure(MozResponse response) {
+      delegate.onFailure(new RuntimeException("TODO"));
+    }
 
-      public FetchSpec build() {
-        return new FetchSpec(b.toString());
-      }
-
-      private void ampersand() {
-        if (first) {
-          return;
-        }
-        b.append('&');
-        first = false;
-      }
-
-      public void setUnread(boolean unread) {
-        ampersand();
-        b.append("unread=");
-        b.append(unread);
-      }
-
-      public void setStatus(String status, boolean not) {
-        ampersand();
-        if (not) {
-          b.append("not_");
-        }
-        b.append("status=");
-        b.append(status);     // Escaping unnecessary for now.
-      }
-
-      private void qualifyAttribute(String qual, String attr) {
-        ampersand();
-        b.append(qual);
-        b.append(attr);
-        b.append('=');
-      }
-
-      public void setMinAttribute(String attr, int val) {
-        qualifyAttribute("min_", attr);
-        b.append(val);
-      }
-
-      public void setMaxAttribute(String attr, int val) {
-        qualifyAttribute("max_", attr);
-        b.append(val);
-      }
-
-      public void setNotAttribute(String attr, String val) {
-        qualifyAttribute("not_", attr);
-        b.append(val);
+    @Override
+    public void addHeaders(HttpRequestBase request, DefaultHttpClient client) {
+      if (ifModifiedSince != -1L) {
+        // TODO: format?
+        request.addHeader("If-Modified-Since", "" + ifModifiedSince);
       }
     }
+  }
+
+  private BaseResource getRelativeResource(final String rel) {
+    return new BaseResource(this.articlesURI.resolve(rel));
   }
 
   /**
    * Use a {@link BasicAuthHeaderProvider} for testing, and an FxA OAuth provider for the real service.
    */
-  public ReadingListClient(final String serviceURL, final AuthHeaderProvider auth) {
-    this.serviceURL = serviceURL;
+  public ReadingListClient(final URI serviceURI, final AuthHeaderProvider auth) {
+    this.serviceURI = serviceURI;
+    this.articlesURI = serviceURI.resolve("articles/");
     this.auth = auth;
   }
 
-  public void getOne(final int id, final ReadingListRecordDelegate delegate, final long ifModifiedSince) throws URISyntaxException {
-    final BaseResource r = new BaseResource(this.serviceURL + "/articles/" + id);
-    r.delegate = new ReadingListResourceDelegate<ReadingListRecordResponse>(r, delegate, ReadingListRecordResponse.FACTORY) {
+  public void getOne(final int id, final ReadingListRecordDelegate delegate, final long ifModifiedSince) {
+    final BaseResource r = getRelativeResource("" + id);
+    r.delegate = new ReadingListRecordResourceDelegate<ReadingListRecordResponse>(r, auth, delegate, ReadingListRecordResponse.FACTORY, ifModifiedSince) {
       @Override
       void onSuccess(ReadingListRecordResponse response) {
         final ReadingListRecord record;
@@ -276,30 +260,12 @@ public class ReadingListClient {
         delegate.onRecordReceived(record);
         delegate.onComplete();
       }
-
-      @Override
-      void onNotModified(ReadingListRecordResponse resp) {
-        delegate.onComplete();
-      }
-
-      @Override
-      void onFailure(MozResponse response) {
-        delegate.onFailure(new RuntimeException("TODO"));
-      }
-
-      @Override
-      public void addHeaders(HttpRequestBase request, DefaultHttpClient client) {
-        if (ifModifiedSince != -1L) {
-          // TODO: format?
-          request.addHeader("If-Modified-Since", "" + ifModifiedSince);
-        }
-      }
     };
   }
 
   public void getAll(final FetchSpec spec, final ReadingListRecordDelegate delegate, final long ifModifiedSince) throws URISyntaxException {
-    final BaseResource r = new BaseResource(spec.getURI(this.serviceURL, "/articles"));
-    r.delegate = new ReadingListResourceDelegate<ReadingListStorageResponse>(r, delegate, ReadingListStorageResponse.FACTORY) {
+    final BaseResource r = new BaseResource(spec.getURI(this.articlesURI));
+    r.delegate = new ReadingListRecordResourceDelegate<ReadingListStorageResponse>(r, auth, delegate, ReadingListStorageResponse.FACTORY, ifModifiedSince) {
       @Override
       void onSuccess(ReadingListStorageResponse response) {
         try {
@@ -313,24 +279,6 @@ public class ReadingListClient {
         }
 
         delegate.onComplete();
-      }
-
-      @Override
-      void onNotModified(ReadingListStorageResponse resp) {
-        delegate.onComplete();
-      }
-
-      @Override
-      void onFailure(MozResponse response) {
-        delegate.onFailure(new RuntimeException("TODO"));
-      }
-
-      @Override
-      public void addHeaders(HttpRequestBase request, DefaultHttpClient client) {
-        if (ifModifiedSince != -1L) {
-          // TODO: format?
-          request.addHeader("If-Modified-Since", "" + ifModifiedSince);
-        }
       }
     };
   }
